ba8494dba4b6ff62c0f6a0c8556667871b8f7dd6
vitormilanez <93198850+vitormilanez@users.noreply.github.com>
2023-10-06 17:44:58 -0300

    b

--- src/Day_3/Formating_Java_Numbers.java
+++ src/Day_3/Formating_Java_Numbers.java
@@ -1,4 +1,13 @@
 package Day_3;
 
+import java.text.NumberFormat;
+
 public class Formating_Java_Numbers {
+    public static void main(String[] args) {
+        NumberFormat pound = NumberFormat.getCurrencyInstance();
+        String productValue = pound.format(1120.80);
+
+        System.out.println(productValue);
+
+    }
 }
